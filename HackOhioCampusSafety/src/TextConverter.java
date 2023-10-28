ea281a032be97cd967a7c22e6082bf5fea3b639a
oberl <oberl@172.28.188.72>
2023-10-28 10:17:55 -0400

    Initial Java commit

---
+++ HackOhioCampusSafety/src/TextConverter.java
@@ -0,0 +1,21 @@
+/**
+ * Converts well-formatted txt file of campus safety to json data.
+ */
+public final class TextConverter {
+
+    /**
+     * No argument constructor--private to prevent instantiation.
+     */
+    private TextConverter() {
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
